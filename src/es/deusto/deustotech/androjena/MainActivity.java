0253e25321ec2ec42d8f405e99219b0c59af4a01
edlectrico <edlectrico@gmail.com>
2013-07-03 18:58:22 +0200

    Testing rules

--- src/es/deusto/deustotech/androjena/MainActivity.java
+++ src/es/deusto/deustotech/androjena/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		generateModel();
+		loadRules();
 	}
 
 	private void generateModel() {
@@ -63,6 +64,19 @@ public class MainActivity extends Activity {
 		testFinalUI.addProperty(viewColor, "DEFAULT");
 	}
 
+	private void loadRules() {
+		String rules = "[adaptViewSize: " +
+				"(?a http://www.w3.org/1999/02/22-rdf-syntax-ns#type ?x) " +
+				"(?x http://www.w3.org/1999/02/22-rdf-syntax-ns#type http://www.w3.org/2002/07/owl#Class) " +
+				"(?t http://www.w3.org/1999/02/22-rdf-syntax-ns#type http://www.lagunfc.com/ontologies/lagunfc.owl#TelephoneTag) " + 
+				"notEqual(?a, ?t) notEqual(?a, http://www.lagunfc.com/ontologies/lagunfc.owl#TelephoneInstance) " +
+				" -> " +
+				"(http://www.lagunfc.com/ontologies/lagunfc.owl#TelephoneInstance http://www.w3.org/1999/02/22-rdf-syntax-ns#type http://www.lagunfc.com/ontologies/lagunfc.owl#TelephoneCall) " +
+				"(http://www.lagunfc.com/ontologies/lagunfc.owl#TelephoneInstance http://www.w3.org/1999/02/22-rdf-syntax-ns#type http://www.lagunfc.com/ontologies/lagunfc.owl#Action) " +
+				"(http://www.lagunfc.com/ontologies/lagunfc.owl#TelephoneInstance http://www.lagunfc.com/ontologies/lagunfc.owl#hasAttribute ?a) " +
+				"(http://www.lagunfc.com/ontologies/lagunfc.owl#TelephoneInstance http://www.lagunfc.com/ontologies/lagunfc.owl#hasTag ?t)]";	
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
